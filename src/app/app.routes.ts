9ea88aceb627f4553343036de44a9afc565f31a1
Thomas <cornu.thomas6@gmail.com>
2025-07-27 21:20:55 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { GamesComponent } from './components/games/games.component';
-import { GameComponent } from './components/game/game.component';
 import { GameSettingsComponent } from './components/game-settings/game-settings.component';
 import { GamePlayComponent } from './components/game-play/game-play.component';
 
@@ -9,7 +8,6 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'games', component: GamesComponent },
-  { path: 'game/:id', component: GameComponent },
   { path: 'settings/:id', component: GameSettingsComponent },
   { path: 'play/:id', component: GamePlayComponent }, // Pour la page de jeu après les paramètres
   { path: '**', redirectTo: '' }
